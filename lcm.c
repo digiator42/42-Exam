a05cbc79105c3457568fc134afe4f75fc58ab6c1
digiator42 <109759977+digiator42@users.noreply.github.com>
2023-01-25 23:01:30 +0400

    lcm

---
+++ lcm.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+unsigned int	lcm(unsigned int a, unsigned int b)
+{
+	int n;
+	
+	if (a > b)
+		n = a;
+	else
+		n = b;
+	while (1)
+	{
+		if (n % a == 0 && n % b == 0)
+			return(n);
+		n++;
+	}
+	
+}
